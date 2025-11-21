c5b54e988fa539f976902e406b607adec46dc2c8
nameELISHA306 <elishavinukonda306@gmail.com>
2025-11-21 07:44:49 +0530

    # WARNING: head commit changed in the meantime
    We have updateda testdata andconfig.properites
    added code

---
+++ src/main/java/amozen/utility/PropertyReader.java
@@ -0,0 +1,37 @@
+package amozen.utility;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.Reader;
+import java.util.Properties;
+
+public class PropertyReader 
+{
+	private static char[] ok;
+	private static FileInputStream s;
+
+	public static void  main(String arg[]) throws Throwable
+	{
+	FileInputStream s=new FileInputStream("");
+	Properties r=new Properties();
+	r.load(s);
+	String value =r.getProperty("Url");
+	String value1= r.getProperty("browser");
+	System.out.println(ok);
+	System.out.println();
+	}
+	//System.out.println(value);
+ 
+//method return type String
+		//method should be String
+		//method Parameterised string
+	public static String elisha (String s1)throws Throwable
+	FileInputStream s=new FileInputStream("");
+	Properties r=new Properties();
+	r.load(s);
+	String value =r.getProperty("Url");
+	String value1= r.getProperty("browser");
+	System.out.println(ok);
+	
+
+		}
